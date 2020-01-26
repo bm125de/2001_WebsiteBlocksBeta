06f6048e9d07f90283eaef87d7f141c441052240
Fahz <>
2020-01-26 18:22:15 +0800

    add header-4 and header-5

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-$('.buttom-1').mouseover(
-    function header2_text_board_a_i_hover() {
-        $('.buttom-1').css('background-color','rgba(255, 255, 255, 1)').css('color','rgba(0,0,0,1)');
-        $('.buttom-1_b').css('top','-10px').css('left','-10px').css('right','-10px').css('bottom','-10px').css('border','rgba(255, 255, 255, 1) solid 1px');
+$('.buttom-1').mouseenter(function (e) {
+        const buttom = e.currentTarget;
+        $(buttom).css('background-color', 'rgba(255, 255, 255, 1)').css('color', 'rgba(0,0,0,1)');
+        $(buttom).find("b").css('top', '-10px').css('left', '-10px').css('right', '-10px').css('bottom', '-10px').css('border', 'rgba(255, 255, 255, 1) solid 1px');
     }
-).mouseout(
-    function header2_text_board_a_i_unhover() {
-        $('.buttom-1').css('background-color','transparent').css('color','rgba(255, 255, 255, 1)');
-        $('.buttom-1_b').css('top','0').css('left','0').css('right','0').css('bottom','0').css('border','rgba(255, 255, 255, 1) solid 0px');
+).mouseleave(
+    function (e) {
+        const button = e.currentTarget;
+        $(button).css('background-color', 'transparent').css('color', 'rgba(255, 255, 255, 1)');
+        console.log(e.currentTarget)
+        $(button).find("b").css('top', '0').css('left', '0').css('right', '0').css('bottom', '0').css('border', 'rgba(255, 255, 255, 1) solid 0px');
     });
 
-$('.buttom-2').mouseover(
-    function header2_text_board_a_i_hover() {
-        $('.buttom-2_i').css('margin-left','10px').css('opacity','100');
-        $('.buttom-2').css('padding-left','20px').css('text-decoration','black solid 2px !important');
-    }
-).mouseout(
-    function header2_text_board_a_i_unhover() {
-        $('.buttom-2_i').css('margin-left','0px').css('opacity','0');
-        $('.buttom-2').css('padding-left','0').css('text-decoration','black solid 0px !important');
-    });
+
+$('.buttom-2').mouseover(function (e) {
+    const dom = e.currentTarget;
+    $(dom).find("i").css('margin-left', '10px').css('opacity', '100');
+    $(dom).css('padding-left', '20px').css('text-decoration', 'black solid 2px !important');
+}).mouseout(function (e) {
+    const dom = e.currentTarget;
+    $(dom).find("i").css('margin-left', '0px').css('opacity', '0');
+    $(dom).css('padding-left', '0').css('text-decoration', 'black solid 0px !important');
+});
